f5375d3c2c897c711e741e12839aa4714ef81419
Fédor <lord.nightmare.1234@gmail.com>
2025-03-10 22:09:13 +0100

    Añadido movimiento básico
    Añadido:
    - Carpeta __test
    - Escena Jugador
    - WASD para moverse

---
+++ Assets/__test/Jugador/Movimiento.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 2.0f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            transform.forward = move;
+        }
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
