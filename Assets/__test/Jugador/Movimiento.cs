c439f9633be0aa0318d62d8b5cb76a2623fcdc29
Fédor <lord.nightmare.1234@gmail.com>
2025-03-10 22:36:26 +0100

    Añadido Dasheo básico
    Añadido:
    - SPACE para dashear

--- Assets/__test/Jugador/Movimiento.cs
+++ Assets/__test/Jugador/Movimiento.cs
@@ -1,12 +1,17 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private bool groundedPlayer;
+    private float dashCooldown;
     [SerializeField] private float playerSpeed = 2.0f;
-
+    [SerializeField] private float sprintValue = 2.0f;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+ 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -15,12 +20,24 @@ public class Movimiento : MonoBehaviour
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
+        float speedMultiplier = Input.GetKey(KeyCode.LeftShift)? sprintValue : 1f;
+        controller.Move(move * Time.deltaTime * playerSpeed * speedMultiplier);
+        if(Input.GetKey(KeyCode.Space)){
+            StartCoroutine(Dash(move));
+        }
+        
         if (move != Vector3.zero)
         {
             transform.forward = move;
         }
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    IEnumerator Dash(Vector3 move){
+        float startTime = Time.time;
+        while(Time.time < startTime + dashTime){
+            controller.Move(move * dashSpeed *Time.deltaTime);
+            yield return null;
+        }
     }
 }
